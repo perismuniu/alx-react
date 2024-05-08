14d99e6cbe3e56b6340e8f95f79d9b3329eb6de6
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-05-08 13:06:04 +0300

    5. Create basic tests with four tests

---
+++ 0x01-react_intro/task_3/dashboard/src/utils.js
@@ -0,0 +1,13 @@
+export function getFullYear() {
+    return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+    return isIndex ? 'Holberton school' : 'Holberton School main dashboard';
+}
+
+export function getLatestNotification() {
+    return (
+        <p><strong>Urgent requirement</strong> - complete by EOD'</p>
+    );
+}
